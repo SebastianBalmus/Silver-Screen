ea01fb386cde065ec6bf4a1993befef98f7ce328
Sebastian-Olav BALMUS <sbalmus@pentalog.com>
2022-07-13 12:43:26 +0300

    added base template and navbar

--- silverscreen/settings.py
+++ silverscreen/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'cinema'
 ]
 
 MIDDLEWARE = [
@@ -122,7 +123,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = 'cinema/static/'
+STATICFILES_DIRS = [
+    BASE_DIR / "cinema/static",
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
