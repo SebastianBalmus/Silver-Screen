23d6cffff900d0a05f0524933401022333905479
Sebastian-Olav BALMUS <sbalmus@pentalog.com>
2022-07-14 14:16:09 +0300

    added movie model

--- cinema/models.py
+++ cinema/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.utils.functional import cached_property
 
 # Create your models here.
+
+
+class Movie(models.Model):
+    name = models.CharField(max_length=50)
+    poster = models.ImageField(upload_to='images')
+    description = models.CharField(max_length=1000)
+    imdb_id = models.CharField(max_length=30)
+    trailer_link = models.CharField(max_length=150)
+    length = models.CharField(max_length=10)
+
+    def __str__(self):
+        return self.name
+
+    @cached_property
+    def imdb_link(self):
+        return 'https://www.imdb.com/title/' + self.imdb_id.__str__()
