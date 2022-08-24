79f3e5603a66dcc210b293a0ad6c1edc4bbac208
SebastianBalmus <balmussebastian@gmail.com>
2022-08-24 17:30:46 +0300

    added seat generation and unit tests for the seats

--- cinema/apps.py
+++ cinema/apps.py
@@ -1,6 +1,12 @@
 from django.apps import AppConfig
+from django.db.models.signals import post_save
 
 
 class CinemaConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'cinema'
+
+    def ready(self):
+        hall_model = self.get_model('CinemaHall')
+        from . import signals
+        post_save.connect(signals.generate_seats, sender=hall_model)
